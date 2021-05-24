81d8b9b6158e75cf16143935589549f9305dd4d6
shaheen5 <miyashaheen1995@gmail.com>
2021-05-24 10:24:29 +0530

    Added : Code To Create AddressBook Contacts Objects

--- AddressBook.js
+++ AddressBook.js
@@ -1,1 +1,24 @@
-console.log("Welcome To Address Book Problem !");
+class AddressBook {
+    //constructor
+    constructor(firstName,lastName,address,city,state,zipCode,phoneNumber,email){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    toString(){
+        return "[ First Name: "+this.firstName+", Last Name: "+this.lastName+", Address: "+this.address+
+                ", City: "+this.city+", State: "+this.state+", Zip Code: "+this.zipCode+", Phone Number: "+
+                this.phoneNumber+", Email: "+this.email+" ]";
+
+    }
+}
+
+let addressbook = new AddressBook("Shaheen","Miya","Gokul Nagar Society","Pune","Maharashtra",411001,
+                    9922123990,"shaheen@gmail.com");
+console.log(addressbook.toString());
